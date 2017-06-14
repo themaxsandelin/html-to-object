a400b4f23a3e3c9c7cf5f291f9919319e9f277a5
Max Sandelin <themaxsandelin@gmail.com>
2017-06-14 16:28:05 +0200

    Removed build process. Doh.

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 
 var fs = require('fs');
 
-function h2o(file, options) {
+function h2o (file, options) {
   options = options || {};
   var html = fs.readFileSync(file, 'utf8');
   var results = extractElement(html);
   var elements = results.elements;
   return elements;
 
-  function extractElement(html) {
+  function extractElement (html) {
     //  Check if the first line contains any whitespace.
     var firstElementIndex = html.indexOf('<');
     if (firstElementIndex) {
@@ -22,7 +22,7 @@ function h2o(file, options) {
     }
 
     // Check if the last line contains any whitespace.
-    var lastLine = html.substring(html.lastIndexOf('>') + 1, html.length);
+    var lastLine = html.substring( html.lastIndexOf('>') + 1, html.length);
     if (lastLine) {
       lastLine = lastLine.replace(' ', '');
       if (lastLine === '\n') {
@@ -33,11 +33,10 @@ function h2o(file, options) {
     var elements = [];
 
     var copy = html;
-
-    var _loop = function _loop() {
+    while (/(<([^>]+)>)/ig.test(copy)) {
       var elementLine = copy.substring(copy.indexOf('<'), copy.indexOf('>') + 1);
-      var elementHasAttributes = elementLine.indexOf(' ') > 1;
-      var nodeEndChar = elementHasAttributes ? ' ' : '>';
+      var elementHasAttributes = (elementLine.indexOf(' ') > 1);
+      var nodeEndChar = (elementHasAttributes) ? ' ':'>';
       var elementNode = elementLine.substring(elementLine.indexOf('<') + 1, elementLine.indexOf(nodeEndChar));
 
       var openTag = '<' + elementNode;
@@ -46,8 +45,8 @@ function h2o(file, options) {
 
       var openTagIndex = copy.indexOf(elementLine) + elementLine.length;
 
-      var hasCloseTag = closeTags.length > 0;
-      var closeTagIndex = void 0;
+      var hasCloseTag = (closeTags.length > 0);
+      var closeTagIndex;
       if (hasCloseTag) {
         if (closeTags.length > 1) {
           closeTags.forEach(function (index) {
@@ -62,11 +61,11 @@ function h2o(file, options) {
       }
 
       var elementStart = copy.indexOf(elementLine);
-      var elementEnd = hasCloseTag ? closeTagIndex + closeTag.length : openTagIndex;
+      var elementEnd = (hasCloseTag) ? (closeTagIndex + closeTag.length) : openTagIndex;
 
       var attributes = [];
       if (elementHasAttributes) {
-        var attributeString = elementLine.substring(elementLine.indexOf(openTag) + openTag.length + 1, elementLine.indexOf('>'));
+        var attributeString = elementLine.substring( elementLine.indexOf(openTag) + openTag.length + 1, elementLine.indexOf('>') );
         attributes = extractAttributes(attributeString);
       }
 
@@ -78,15 +77,11 @@ function h2o(file, options) {
         node: elementNode,
         attributes: attributes,
         children: children,
-        content: !options.contentWhitespace ? trimContentWhitespace(parsed.leftover) : parsed.leftover
-      };
+        content: (!options.contentWhitespace) ? trimContentWhitespace(parsed.leftover):parsed.leftover
+      }
 
       elements.push(element);
       copy = copy.substring(0, elementStart) + copy.substring(elementEnd, copy.length);
-    };
-
-    while (/(<([^>]+)>)/ig.test(copy)) {
-      _loop();
     }
 
     return {
@@ -95,7 +90,7 @@ function h2o(file, options) {
     };
   }
 
-  function trimContentWhitespace(string) {
+  function trimContentWhitespace (string) {
     var temp = string;
     // Remove linebreaks
     while (temp.indexOf('\n') > -1) {
@@ -113,7 +108,7 @@ function h2o(file, options) {
     return string.substring(string.indexOf(firstChar), string.indexOf(lastChar) + 1);
   }
 
-  function extractAttributes(string) {
+  function extractAttributes (string) {
     var attributes = [];
     var strings = string.split(' ');
     strings.forEach(function (attr) {
@@ -121,12 +116,12 @@ function h2o(file, options) {
       attributes.push({
         name: parts[0],
         value: parts[1].replace(/"/g, '').replace(/'/g, '')
-      });
+      })
     });
     return attributes;
   }
 
-  function getAllOccurrences(part, string) {
+  function getAllOccurrences (part, string) {
     var occurrences = [];
     var temp = string;
     while (temp.indexOf(part) > -1) {
